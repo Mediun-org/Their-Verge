065ea4d595f95c0708362881d6f10a440ee3a651
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2019-11-22 20:04:47 +0200

    comments in server

--- post/server/index.js
+++ post/server/index.js
@@ -11,33 +11,33 @@ const Author = db.Author;
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/article/:id', (req, res) => {
-//   var id = req.params.id;
-//   console.log('the id in post is: ', id);
-//   var arr = [];
-//   db.selectAll(
-//     Article,
-//     (err, art) => {
-//       console.log('Hendd', art[0]);
-//       arr.push(art[0]);
-//       db.selectAll(
-//         Author,
-//         (err, author) => {
-//           arr.push(author[0]);
-//           db.selectAll(Article, (err, arts) => {
-//             arr.push(arts);
-//             // var result = arr.toJSON();
-//             // res.json(arr);
-//             console.log('this the arr in server: ', arr);
-//             res.status(202).send(arr);
-//           });
-//         },
-//         art[0].authorId
-//       );
-//     },
-//     id
-//   );
-// });
+app.get('/article/:id', (req, res) => {
+  var id = req.params.id;
+  console.log('the id in post is: ', id);
+  var arr = [];
+  db.selectAll(
+    Article,
+    (err, art) => {
+      console.log('Hendd', art[0]);
+      arr.push(art[0]);
+      db.selectAll(
+        Author,
+        (err, author) => {
+          arr.push(author[0]);
+          db.selectAll(Article, (err, arts) => {
+            arr.push(arts);
+            // var result = arr.toJSON();
+            // res.json(arr);
+            console.log('this the arr in server: ', arr);
+            res.status(202).send(arr);
+          });
+        },
+        art[0].authorId
+      );
+    },
+    id
+  );
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../public'));
