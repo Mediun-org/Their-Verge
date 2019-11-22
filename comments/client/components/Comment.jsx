dbeb6fe9f426615c4ac1f14927fc4a9a3183614d
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2019-11-22 19:50:27 +0200

    comments done

--- comments/client/components/Comment.jsx
+++ comments/client/components/Comment.jsx
@@ -1,34 +1,24 @@
-import React from "react";
-
+import React from 'react';
 
 class Comment extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-
-    // };
-
   }
 
-  render(){
-    return(
-      <div className = 'commentItem'>
-
-				<div className = 'commentItemHeader'>
-					<img className = 'autherAvatar' src = {this.props.comment.profilePic}/>
-					<h5 className = 'autherName'>{this.props.comment.autherName}</h5>
-          
-				</div>
-        
-        
-        <p className = 'commentbody'>{this.props.comment.body}</p>
-				<p>Posted on 	<a>{this.props.comment.createdAt}</a> </p>
-
+  render() {
+    return (
+      <div className='commentItem'>
+        <div className='commentItemHeader'>
+          <img className='autherAvatar' src={this.props.comment.imgUrl} />
+          <h5 className='autherName'>{this.props.comment.name}</h5>
+        </div>
+        <p className='commentbody'>{this.props.comment.text}</p>
+        <p>
+          Posted on <a>{this.props.comment.date}</a>
+        </p>
       </div>
-    )
+    );
   }
 }
 
 export default Comment;
-
-
