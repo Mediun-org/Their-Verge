7c584817b2b6292e15c04f639f3c53a402846b61
hend-isleem <hendisleem@gmail.com>
2019-11-23 10:32:56 +0200

    testing post

--- post/client/src/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import Article from '../index';
-it('renders without crashing', () => {
-  const div = document.getElementById("post");
-  ReactDOM.render(<Article />, div);
-});
